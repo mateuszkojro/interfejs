4cf9db494440b397e7ac5c5eca02a4485641dd55
mateuszkojro <mateusz.kojro@unilodz.eu>
2020-01-05 23:32:47 +0100

    obsluga wyborow

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 let nie_wiem = document.getElementById("drugi");
 let wiem = document.getElementById("pierwszy");
 
-let wybor;
+let wybor = "";
 
-//nie_wiem.onclick = nie_wiem_click();
-//wiem.onclick = wiem_click();
-
-
-//create_fluff();
 console.log("i am herer");
 
-//ukrywam sekcje z pytaniami powinienem w funkcji gdzies
 document.getElementById("q1").style.display = "none";
 document.getElementById("q2").style.display = "none";
 document.getElementById("q3").style.display = "none";
@@ -21,6 +15,107 @@ document.getElementById("koniec").style.display = "none";
 for(let i = 0; i < document.getElementsByClassName("in").length;i++){
 	document.getElementsByClassName("in")[i].style.display = "none";
 }
+
+
+function submit(){
+
+	let string = "<h1> Gratulacje! </h1><h2> wybralas/es </h2>" + wybor;
+	
+	document.getElementById("endgame").innerHTML = string;
+	
+
+}
+
+
+function button1_click() {
+
+	wybor = "all";
+	show_the_choices(wybor);
+	
+}
+
+function button2_click() {
+
+	document.getElementById("q1").style.display = "";
+	window.location.assign("#quiz");
+
+}
+
+function button3_click() {
+
+	wybor = "";
+	show_the_choices(wybor);
+	
+}
+
+function button4_click() {
+	
+	document.getElementById("q2").style.display = "";
+	window.location.assign("#q2");
+
+}
+
+function button5_click() {
+
+	wybor = "";
+	show_the_choices(wybor);
+
+}
+
+function button6_click() {
+
+	document.getElementById("q3").style.display = "";
+	window.location.assign("#q3");
+
+}
+
+function button7_click() {
+	
+	document.getElementById("q4").style.display = "";
+	window.location.assign("#q4");
+
+}
+
+function button8_click() {
+
+	wybor = "";
+	show_the_choices(wybor);
+
+}
+
+function button9_click() {
+
+	wybor = "";
+	show_the_choices(wybor);
+
+}
+
+function button10_click() {
+
+	wybor = "";
+	show_the_choices(wybor);
+
+}
+
+
+function hide(obj) {
+
+	document.getElementById(obj).style.display = "none";
+
+}
+
+function logout(){
+	
+	window.location.assign("login.html");
+
+}
+
+
+function login() {
+    
+	window.location.assign("quiz.html");
+
+} 
 
 function show_the_choices(input) {
 
@@ -88,94 +183,3 @@ function show_the_choices(input) {
 
 
 }
-
-
-function button1_click() {
-
-	console.log("button 1 click");
-	//write_cookie("fuck off you motherfucker");
-	window.location.assign("wybierz.html");
-
-
-	wybor = "all";
-	show_the_choices(wybor);
-	
-}
-
-function button2_click() {
-
-	console.log("button 2 click");
-	document.getElementById("q1").style.display = "";
-	window.location.assign("#quiz");
-
-
-}
-
-function button3_click() {
-
-	wybor = "";
-	show_the_choices(wybor);
-	
-}
-
-function button4_click() {
-	
-	document.getElementById("q2").style.display = "";
-	window.location.assign("#q2");
-
-}
-
-function button5_click() {
-
-	wybor = "";
-	show_the_choices(wybor);
-
-}
-
-function button6_click() {
-	document.getElementById("q3").style.display = "";
-	window.location.assign("#q3");
-}
-
-function button7_click() {
-	document.getElementById("q4").style.display = "";
-	window.location.assign("#q4");
-}
-
-function button8_click() {
-
-	wybor = "";
-	show_the_choices(wybor);
-
-}
-
-function button9_click() {
-
-	wybor = "";
-	show_the_choices(wybor);
-
-}
-
-function button10_click() {
-
-	wybor = "";
-	show_the_choices(wybor);
-
-}
-
-
-function hide(obj) {
-	document.getElementById(obj).style.display = "none";
-	//window.location.assign("#q3");
-}
-
-function logout(){
-	window.location.assign("login.html");
-}
-
-
-function login() {
-    
-	window.location.assign("quiz.html");
-
-} 
